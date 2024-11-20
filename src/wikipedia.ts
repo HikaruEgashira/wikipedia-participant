ff0dae3128802e2fccb9147609d66944d198d7a8
hikae <account@egahika.dev>
2024-11-21 01:42:42 +0900

    add ja baseUrl

--- src/wikipedia.ts
+++ src/wikipedia.ts
@@ -4,6 +4,7 @@ export async function wikipediaSearch(prompt: string) {
   const tool = new WikipediaQueryRun({
     topKResults: 3,
     maxDocContentLength: 4000,
+    baseUrl: "https://ja.wikipedia.org/w/api.php",
   });
 
   const res = await tool.invoke(prompt);
